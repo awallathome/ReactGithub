a936a005360f76421d03cb217ce14680ccb5515f
Adam Wallis <adam.wallis@me.com>
2022-08-05 14:23:37 -0500

    update to close tag

--- api_app/src/detailView.js
+++ api_app/src/detailView.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 function DetailView({detail, setIsLoading}) {
 
+
   return (
     <div className="bio-card">
+      <div>
+        <a href="#" id="closeBtn" onClick={()=> {setIsLoading(true)}} >x</a>
+      </div>
       <div display="inline-block"><img src={detail.avatar_url} alt="face" width="150" height="150" /></div>
       <div>{detail.name} ({detail.login})</div>
       <div>{detail.location}</div>
       <div >{detail.bio}</div>
       <div>Followers: {detail.followers}</div>
       <div>Folowing: {detail.following}</div>
-      <button id="closeBtn" onClick={()=> {setIsLoading(true)}} float="right">X</button>
+      <a href={detail.html_url} target="_blank">{detail.html_url}</a>
+      
     </div>
   )
 
