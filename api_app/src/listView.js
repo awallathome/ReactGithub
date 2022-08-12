5d48d32f3e38f1b43d566d02525664d7e7930a2d
awallathome <adam.wallis@me.com>
2022-08-12 10:36:59 -0500

    moved onclick from row to avatar

--- api_app/src/listView.js
+++ api_app/src/listView.js
@@ -1,67 +1,72 @@
-import React, { useState } from 'react';
-import './App.css';
-import DetailView from './detailView';
-import axios from 'axios';
+import React, { useState } from "react";
+import "./App.css";
+import DetailView from "./detailView";
+import axios from "axios";
 
 function ListView({ data }) {
-
-  const [user, setUser ] = useState("")
+  const [user, setUser] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   const styles = {
     avatar: {
-      width: '50px',
-      height: '50px',
+      width: "50px",
+      height: "50px"
     },
     headerColumn: {
-      width: '33%',
+      width: "33%"
     },
     contributors: {
-      width:'100%',
+      width: "100%"
     }
-  }
- 
+  };
+
   function getUserDetail(item) {
     axios({
       method: "get",
       url: `https://api.github.com/users/${item.login}`
     })
-      .then((res) => {
+      .then(res => {
         // console.log("user", res.data)
-        setUser(res.data)
-       
+        setUser(res.data);
       })
-      .then(()=>{
+      .then(() => {
         setIsLoading(false);
-      })
-
+      });
   }
 
   function openDetail(item) {
     setUser(item);
     getUserDetail(item);
   }
- 
+
   return (
     <div>
-      {!isLoading ? <DetailView detail={user} setIsLoading={setIsLoading}/> : null}
+      {!isLoading ? (
+        <DetailView detail={user} setIsLoading={setIsLoading} />
+      ) : null}
       <table style={styles.contributors}>
-        <tr >
+        <tr>
           <th>Avatar</th>
           <th>Login</th>
           <th># Contributions</th>
         </tr>
         {data.map(item => (
-          <tr onClick={() => openDetail(item)}>
-            <th style={styles.headerColumn} ><img src={item.avatar_url} alt="face" style={styles.avatar} /></th>
-            <th >{item.login}</th>
-            <th >{item.contributions}</th>
+          <tr>
+            <th style={styles.headerColumn}>
+              <img
+                onClick={() => openDetail(item)}
+                src={item.avatar_url}
+                alt="face"
+                style={styles.avatar}
+              />
+            </th>
+            <th>{item.login}</th>
+            <th>{item.contributions}</th>
           </tr>
         ))}
       </table>
-      
     </div>
-  )
-};
+  );
+}
 
 export default ListView;
